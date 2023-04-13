6f9606842302d4493aa2ba5db3f28bcae3f030c3
Nakadzima9 <pascal.german@yandex.ru>
2023-04-13 16:30:50 +0600

    feat: removed unused fields in User model and remake UserManager

--- users/models.py
+++ users/models.py
@@ -9,46 +9,36 @@ from django.core.mail import send_mail
 from django.db import models
 from django.utils import timezone
 
-from cmsapp.models import Department, ScheduleType, Group
-
 
 class SuperUser(BaseUserManager):
 
-    def create_user(self, email, **extra_fields):
-        if not extra_fields.get('without_generate_password', False):
-            if not email:
-                raise ValueError("You must provide an email")
-            generate_password = self.make_random_password()
-            user = self.model(
-                email=self.normalize_email(email),
-                **extra_fields
-            )
-            user.set_password(generate_password)
-            user.fio = user.first_name +" "+ user.last_name
-            user.save()
-            send_mail("Пароль от CRM",
-                      f"Ваши данные для входа в CRM:\nПочта: {user.email}\nПароль: {generate_password}",
-                      "from@crm-project.com",
-                      [user.email],
-                      fail_silently=False,
-                      )
-            return user
-        extra_fields.pop('without_generate_password')
-        user = self.model(email=self.normalize_email(email), **extra_fields)
-        user.fio = user.first_name + " " + user.last_name
-        return user
-
-    def create_superuser(self, email, password):
-        user = self.create_user(
-            email=email,
-            without_generate_password=True
-        )
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a user with the given email and password.
+        """
+        if not email:
+            raise ValueError("The Email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
-        user.is_superuser = True
-        user.is_staff = True
-        user.user_type = 'admin'
         user.save()
         return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        if extra_fields.get("is_active") is not True:
+            raise ValueError("Superuser must have is_active=True")
+        return self.create_user(email, password, **extra_fields)
 
 
 def user_directory_path(instance, filename):
@@ -80,7 +70,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     TYPE_ROLE_CHOICES = [
         ("admin", "Admin"),
         ("manager", "Manager"),
-        ("mentor", "Mentor"),
     ]
     email = models.EmailField(unique=True, verbose_name="Почта")
     first_name = models.CharField(max_length=50, verbose_name="Имя")
@@ -89,15 +78,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     # unique_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     # image = models.ForeignKey(UserImage, on_delete=models.CASCADE, blank=True, null=True, verbose_name="Аватар")
     image = models.ImageField(upload_to=user_directory_path, default='default.jpg', blank=True, verbose_name="Аватар")
-    linkedin = models.URLField(blank=True, verbose_name="Ссылка на Linkedin")
     user_type = models.CharField(max_length=255, choices=TYPE_ROLE_CHOICES, verbose_name="Тип пользователя")
-    department = models.ForeignKey(Department, on_delete=models.CASCADE, blank=True, null=True,
-                                   related_name='department', verbose_name='Департамент')
-    # group = models.ForeignKey(Group, on_delete=models.CASCADE, blank=True, null=True, related_name='group',
-    #                            verbose_name='Группа')
-    patent_number = models.PositiveIntegerField(blank=True, default=0, verbose_name="Номер патента")
-    patent_start = models.DateField(blank=True, null=True, verbose_name="Срок действия патента")
-    patent_end = models.DateField(blank=True, null=True, verbose_name="Срок окончания патента")
     is_staff = models.BooleanField(default=False, verbose_name="Сотрудник")
     is_active = models.BooleanField(default=True, verbose_name="Активен")
     is_superuser = models.BooleanField(default=False, verbose_name="Суперь пользователь")
@@ -108,43 +89,9 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = SuperUser()
 
-    # def save(self, *args, **kwargs):
-    #     try:
-    #         update_status = kwargs.pop("update")
-    #     except KeyError:
-    #         update_status = False
-    #
-    #     if not update_status:
-    #         generate_password = User.objects.make_random_password()
-    #         send_mail(
-    #             "Ваши данные для входа в CRM:",
-    #             f"Ваши данные для входа в CRM:\n"
-    #             f"Пароль: {generate_password}",
-    #             "from@crm-project.com",
-    #             [self.email],
-    #         )
-    #         self.set_password(generate_password)
-    #     super(User, self).save(*args,**kwargs)
-
     def __str__(self):
         return f"{self.email}"
 
     class Meta:
         verbose_name = 'Системный пользователь'
         verbose_name_plural = "Системные пользователи"
-
-
-class OTP(models.Model):
-    user = models.ForeignKey("User", on_delete=models.CASCADE)
-    unique_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    status = models.BooleanField(default=False)
-    password_life_time = models.DateTimeField(auto_now_add=True)
-    code = models.CharField(max_length=6)
-    created_time = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"User: {self.user} - OTP: {self.code}"
-
-    class Meta:
-        verbose_name = 'OTP'
-        verbose_name_plural = 'OTP'
