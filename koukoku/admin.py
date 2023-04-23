f8da0bbb94bab2b6c28d43e241c3292ef0d6f12c
Nakadzima9 <pascal.german@yandex.ru>
2023-04-24 00:20:19 +0600

    feat: added models and forms koukoku

--- koukoku/admin.py
+++ koukoku/admin.py
@@ -1,3 +1,66 @@
 from django.contrib import admin
 
-# Register your models here.
+from koukoku.models import (
+    Region,
+    City,
+    District,
+    Communication,
+    Document,
+    TargetPurpose,
+    Infrastructure,
+    PaymentTerm,
+    Estate, Location)
+
+
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+
+
+class DistrictAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'city', 'district', 'region')
+
+
+class CommunicationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+class TargetPurposeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+class InfrastructureAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+class PaymentTermAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+class EstateAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'plot_area', 'house_area', 'deal_type', 'price', 'currency', 'phone', 'location',
+                    'target', 'communications', 'documents', 'infrastructure', 'payment_term'
+                    )
+
+
+admin.site.register(Region, RegionAdmin)
+admin.site.register(City, CityAdmin)
+admin.site.register(District, DistrictAdmin)
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Communication, CommunicationAdmin)
+admin.site.register(Document, DocumentAdmin)
+admin.site.register(TargetPurpose, TargetPurposeAdmin)
+admin.site.register(Infrastructure, InfrastructureAdmin)
+admin.site.register(PaymentTerm, PaymentTermAdmin)
+admin.site.register(Estate, EstateAdmin)
--- koukoku/models.py
+++ koukoku/models.py
@@ -1,3 +1,136 @@
 from django.db import models
 
-# Create your models here.
+
+class Region(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Название области")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Область"
+        verbose_name_plural = "Области"
+
+
+class City(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Название города")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Город"
+        verbose_name_plural = "Города"
+
+
+class District(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Название района")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Район"
+        verbose_name_plural = "Районы"
+
+
+class Location(models.Model):
+    city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True, verbose_name="Город")
+    district = models.ForeignKey(District, on_delete=models.SET_NULL, null=True, verbose_name="Район")
+    region = models.ForeignKey(Region, on_delete=models.SET_NULL, null=True, verbose_name="Область")
+
+    def __str__(self):
+        return f"Область: {self.region} - город: {self.city} - район: {self.district}"
+
+    class Meta:
+        verbose_name_plural = "Локации"
+
+
+class Communication(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Коммуникации")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Коммуникации"
+
+
+class Document(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Документы")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Документы"
+
+
+class TargetPurpose(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Целовое назначение")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Целовое назначение"
+
+
+class Infrastructure(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Инфраструктура")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Инфраструктура"
+
+
+class PaymentTerm(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Условие оплаты")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Условие оплаты"
+
+
+class Estate(models.Model):
+
+    CURRENCY_CHOICES = (
+        (1, "USD"),
+        (2, "СОМ"),
+        (3, "Договорная"),
+    )
+
+    DEAL_TYPE_CHOICES = (
+        (1, "Продаю участок"),
+        (2, "Продаю дом"),
+        (3, "Продаю под снос"),
+        (4, "Аренда участка"),
+        (5, "Куплю участок/дом"),
+    )
+
+    name = models.CharField(max_length=50, verbose_name="Название")
+    description = models.TextField(verbose_name="Описание")
+    image = models.ImageField(verbose_name="Изображение")
+    plot_area = models.PositiveSmallIntegerField(null=True, verbose_name="Площадь участка")
+    house_area = models.PositiveSmallIntegerField(null=True, verbose_name="Площадь дома")
+    deal_type = models.CharField(max_length=30, choices=DEAL_TYPE_CHOICES, default=None, verbose_name="Тип сделки")
+    price = models.PositiveIntegerField(null=True, verbose_name="Цена")
+    currency = models.CharField(max_length=20, choices=CURRENCY_CHOICES, default=None, verbose_name="Валюта")
+    phone = models.CharField(max_length=9, verbose_name="Номер телефона")
+    location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True, verbose_name="Расположение")
+    target = models.ForeignKey(TargetPurpose, on_delete=models.SET_NULL, null=True, verbose_name="Целевое назначение")
+    communications = models.ForeignKey(Communication, on_delete=models.SET_NULL, null=True, verbose_name="Коммуникации")
+    documents = models.ForeignKey(Document, on_delete=models.SET_NULL, null=True, verbose_name="Документы")
+    infrastructure = models.ForeignKey(Infrastructure, on_delete=models.SET_NULL, null=True,
+                                       verbose_name="Инфраструктура")
+    payment_term = models.ForeignKey(PaymentTerm, on_delete=models.SET_NULL, null=True, verbose_name="Условия оплаты")
+
+    def __str__(self):
+        return f"Объявление: {self.name}"
+
+    class Meta:
+        verbose_name_plural = "Объявления"
