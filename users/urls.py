145f83d80a67f62a502d6fa35121985c6d9f54fc
Nakadzima9 <pascal.german@yandex.ru>
2023-04-26 02:56:48 +0600

    feat: added profile view

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 
-from users.views import RegisterView
+from users.views import RegisterView, ProfileUpdateView
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
+    path('<int:pk>/edit/', ProfileUpdateView.as_view(), name='user_profile_edit'),
     path('register/', RegisterView.as_view(), name='register')
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import UpdateView, DetailView
 
-from users.forms import RegisterForm
+from users.forms import RegisterForm, UserUpdateForm
 from django.shortcuts import render, redirect
 from django.views import View
 
@@ -29,3 +32,14 @@ class RegisterView(View):
             'form': form
         }
         return render(request, self.template_name, context)
+
+
+class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+    model = get_user_model()
+    form_class = UserUpdateForm
+    template_name = 'profile_update.html'
+    success_url = reverse_lazy('profile')
+    queryset = get_user_model().objects.all()
+
+    def get_object(self):
+        return self.request.user
